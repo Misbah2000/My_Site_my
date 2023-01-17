8be11f51a5cb17d42a5b6390a1f248bb972f4ca1
Misba Mansuri <115460562+Misbah2000@users.noreply.github.com>
2023-01-18 00:26:42 +0530

    nav responsible

--- app.js
+++ app.js
@@ -33,3 +33,21 @@ const sendEmail = (event) => {
 };
 
 contactForm.addEventListener('submit', sendEmail);
+
+
+// NAVIGETION BAR
+const menuBtn = document.querySelector(".menu-icon i");
+const cancelBtn = document.querySelector(".cancel-icon");
+const items = document.querySelector(".nav-items");
+
+menuBtn.onclick = ()=>{
+    items.classList.add("active");
+    menuBtn.classList.add("hide");
+    cancelBtn.classList.add("show");
+}
+
+cancelBtn.onclick = ()=>{
+    items.classList.remove("active");
+    menuBtn.classList.remove("hide");
+    cancelBtn.classList.remove("show");
+}
