a3222f5346ae4559104e6561cae644fec92c406d
Misba Mansuri <115460562+Misbah2000@users.noreply.github.com>
2023-01-17 20:18:52 +0530

    chenges commit

--- app.js
+++ app.js
@@ -9,21 +9,14 @@ const sendEmail = (event) => {
     event.preventDefault();
 
     // add and remove color
-    if (
-        contactName.value === '' || contactEmail.value === '' || message.value === '' ) {
-        contactMessage.classList.remove('color-light');
-        contactMessage.classList.add('color-dark');
-
-        // show message
-        contactMessage.textContent = 'Write all the input fields';
-    } else {
+    
         // seviceID, templateID, #form, publickey
         emailjs.sendForm('service_79584ur', 'template_rb1g7hc', '#contact-form', 'qXBvcT_llsI-ZxvN0')
 
             .then(() => {
                 // show message and add color
                 contactMessage.classList.add('color-light');
-                contactMessage.textContent = 'Message send ✔️';
+                contactMessage.textContent = 'Message send';
 
                 setTimeout(() => {
                     contactMessage.textContent = '';
@@ -36,7 +29,7 @@ const sendEmail = (event) => {
         contactName.value = '';
         contactEmail.value = '';
         message.value = '';
-    }
+    
 };
 
 contactForm.addEventListener('submit', sendEmail);
